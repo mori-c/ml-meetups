20a70c8553b5a6fdab407f9aba614118e6d64926
mori-c <moriconstant@gmail.com>
2019-04-29 20:56:46 -0400

    Step 5 (Http requests using Axios)todoform.jsx

--- src/app/components/TodoForm.jsx
+++ src/app/components/TodoForm.jsx
@@ -4,17 +4,61 @@ import { Redirect } from 'react-router';
 import './App.scss';
 
 const statusEnum = {
-  DONE : "Done",
-  TODO : "To Do",
-  InProgress : "In Progress"
+  DONE: "Done",
+  TODO: "To Do",
+  InProgress: "In Progress"
 }
 
-export class TodoForm extends React.Component{
+export class TodoForm extends React.Component
+{
 
-  render() {
+  // react.constructor METHODS {
+  // add this.super
+  // def super.api( http://localhost:7777)
+  // }
+
+  constructor ( props )
+  {
+    super( props );
+    const apiurl = "http://localhost:7777";
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind( this );
+    this.handSubmit = this.handleSubmit.bind( this );
+  }
+  handleChange( event )
+  {
+    this.setState( { value: event.target.value } );
+  }
+  handleSubmit( event )
+  {
+    alert( 'Yo, name was submitted: ' + this.state.value );
+    event.preventDefault();
+  }
+  render()
+  {
+    const element = <h1> TodoForm</h1>;
     return (
-      <div>
+      <div> {element}
+        <form onSubmit={this.handleSubit}>
+          <label>
+            Name:
+            <input type="text" name="name" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <br />
+          Description:
+          <input type="text" name="description" />
+          <br />
+          Select:
+          <select name="items">
+            <option value="eta1">ETA 1</option>
+            <option value="eta2">ETA 2</option>
+          </select>
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
-    )
+    );
   }
 }
+
+
